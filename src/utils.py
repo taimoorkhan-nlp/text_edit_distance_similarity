a7597de83362f39df8c71660d70af07d5c8f653b
Johannes Kiesel <Johannes.Kiesel@gesis.org>
2025-08-29 16:02:47 +0200

    try solve formatting errors

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,12 @@
 import random
 import string
 
+
 # 1. Simple Edit distance
-def simple_edit_distance(text1, text2, level='c'):
-    if level == 'w':
-        text1 = text1.split(' ')
-        text2 = text2.split(' ')
+def simple_edit_distance(text1, text2, level="c"):
+    if level == "w":
+        text1 = text1.split(" ")
+        text2 = text2.split(" ")
     m, n = len(text1), len(text2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
 
@@ -25,11 +26,12 @@ def simple_edit_distance(text1, text2, level='c'):
 
     return dp[m][n]
 
+
 # 2. Levenstein edit distance
-def levenshtein_edit_distance(text1, text2, level='c'):
-    if level == 'w':
-        text1 = text1.split(' ')
-        text2 = text2.split(' ')
+def levenshtein_edit_distance(text1, text2, level="c"):
+    if level == "w":
+        text1 = text1.split(" ")
+        text2 = text2.split(" ")
     m, n = len(text1), len(text2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
 
@@ -47,17 +49,19 @@ def levenshtein_edit_distance(text1, text2, level='c'):
             else:
                 substitution_cost = 2
                 dp[i][j] = min(
-                    dp[i - 1][j] + 1,                  # Deletion
-                    dp[i][j - 1] + 1,                  # Insertion
-                    dp[i - 1][j - 1] + substitution_cost  # Substitution
+                    dp[i - 1][j] + 1,  # Deletion
+                    dp[i][j - 1] + 1,  # Insertion
+                    dp[i - 1][j - 1] + substitution_cost,  # Substitution
                 )
 
     return dp[m][n]
 
-def damerau_levenshtein_distance(text1, text2, level='c'):
-    if level == 'w':
-        text1 = text1.split(' ')
-        text2 = text2.split(' ')
+
+# 3. Damerau Levenstein edit distance
+def damerau_levenshtein_distance(text1, text2, level="c"):
+    if level == "w":
+        text1 = text1.split(" ")
+        text2 = text2.split(" ")
         
     m, n = len(text1), len(text2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
@@ -75,61 +79,67 @@ def damerau_levenshtein_distance(text1, text2, level='c'):
 
             # Minimum cost of insertion, deletion, substitution
             dp[i][j] = min(
-                dp[i - 1][j] + 1,       # Deletion
-                dp[i][j - 1] + 1,       # Insertion
-                dp[i - 1][j - 1] + cost  # Substitution
+                dp[i - 1][j] + 1,  # Deletion
+                dp[i][j - 1] + 1,  # Insertion
+                dp[i - 1][j - 1] + cost,  # Substitution
             )
 
             # Check for transpositions
-            if i > 1 and j > 1 and text1[i - 1] == text2[j - 2] and text1[i - 2] == text2[j - 1]:
-                dp[i][j] = min(
-                    dp[i][j],
-                    dp[i - 2][j - 2] + cost  # Transposition
-                )
+            if (
+                i > 1
+                and j > 1
+                and text1[i - 1] == text2[j - 2]
+                and text1[i - 2] == text2[j - 1]
+            ):
+                dp[i][j] = min(dp[i][j], dp[i - 2][j - 2] + cost)  # Transposition
 
     return dp[m][n]
 
 
-def randomize_text(text, revs = 1):
+def randomize_text(text, revs=1):
     for rev in range(revs):        
         op = random.randint(1, 4)
-        if op == 1: #insertion
+        if op == 1:  # insertion
             text = insertion(text)
 
-        if op == 2: #deletion
+        if op == 2:  # deletion
             text = deletion(text)
         
-        if op == 3: #substitution
+        if op == 3:  # substitution
             text = substitution(text)  
         
-        if op == 4: #transposition
+        if op == 4:  # transposition
             text = transposition(text)            
         
     return text
 
-#operations
+
+# Operations
 def insertion(text):
-    loc = random.randint(0,len(text)-1)
-    allchar = list(string.ascii_lowercase)+list(string.ascii_uppercase)
+    loc = random.randint(0, len(text) - 1)
+    allchar = list(string.ascii_lowercase) + list(string.ascii_uppercase)
     nch = random.choice(allchar)
-    newText = str(text[0:loc]) + nch + str(text[loc: len(text)])
+    newText = str(text[0:loc]) + nch + str(text[loc : len(text)])
     return newText
+
 
 def deletion(text):
-    loc = random.randint(0, len(text)-1)
-    newText = str(text[0:loc]) + str(text[loc+1:len(text)])
+    loc = random.randint(0, len(text) - 1)
+    newText = str(text[0:loc]) + str(text[loc + 1 : len(text)])
     return newText
+
 
 def substitution(text):
-    loc = random.randint(0, len(text)-1)
-    allchar = list(string.ascii_lowercase)+list(string.ascii_uppercase)
+    loc = random.randint(0, len(text) - 1)
+    allchar = list(string.ascii_lowercase) + list(string.ascii_uppercase)
     nch = random.choice(allchar)
-    newText = str(text[0:loc]) + nch + str(text[loc+1:len(text)])
+    newText = str(text[0:loc]) + nch + str(text[loc + 1 : len(text)])
     return newText
 
+
 def transposition(text):
-    loc = random.randint(0, len(text)-1-1)
+    loc = random.randint(0, len(text) - 1 - 1)
     ch = text[loc]
-    text = str(text[0:loc]+text[loc+1]+text[loc]+text[loc+2:])
+    text = str(text[0:loc] + text[loc + 1] + text[loc] + text[loc + 2 :])
     newText = text
     return newText
